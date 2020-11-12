e479437788071808247d4594d49aa7171c110750
SosoM1k0r31z31 <sosoplayzz@gmail.com>
2020-11-12 09:02:43 +0300

    .

--- resources/documentation/Unused/misc-stuff/ewmx-bot.py
+++ resources/documentation/Unused/misc-stuff/ewmx-bot.py
@@ -5,7 +5,7 @@ from time import sleep
 
 # don't worry about this part :trorr:
 
-TOKEN = ('NzQ0Mjg2MzAzMzY0ODQxNTA1.XzhAog.OUNDrnf0THQseopKPCeoZ6cMyiY')
+TOKEN = ('')
 
 bot = commands.Bot(command_prefix='%')
 
@@ -57,7 +57,7 @@ async def pecorine(ctx):
                                                                                                         print("samplestring check done")
         sleep(5)
         for i in range(10):
-            await ctx.send("https://images-wixmp-ed30a86b8c4ca887773594c2.wixmp.com/f/ead65e88-502c-4a97-9113-44a80af5452f/de18pej-e3d266e1-d6cc-4b10-b9bd-666489904474.png/v1/fill/w_600,h_829,q_80,strp/pecorine_by_wanipurin_de18pej-fullview.jpg?token=eyJ0eXAiOiJKV1QiLCJhbGciOiJIUzI1NiJ9.eyJzdWIiOiJ1cm46YXBwOiIsImlzcyI6InVybjphcHA6Iiwib2JqIjpbW3siaGVpZ2h0IjoiPD04MjkiLCJwYXRoIjoiXC9mXC9lYWQ2NWU4OC01MDJjLTRhOTctOTExMy00NGE4MGFmNTQ1MmZcL2RlMThwZWotZTNkMjY2ZTEtZDZjYy00YjEwLWI5YmQtNjY2NDg5OTA0NDc0LnBuZyIsIndpZHRoIjoiPD02MDAifV1dLCJhdWQiOlsidXJuOnNlcnZpY2U6aW1hZ2Uub3BlcmF0aW9ucyJdfQ.TByWvZjJmEh_a-6NsCb1w4CRwS-9XAavQ1pz4RLc8Cc")
+            await ctx.send("https://i.pinimg.com/736x/ad/bd/1f/adbd1f66b077d5a4ba3de2368c897a66.jpg")
 
         for i in range(100):
             print(f"{i} {msgg}")
@@ -100,7 +100,7 @@ async def pecorine(ctx):
                                                                                                         print("samplestring check done")
         sleep(5)
         for i in range(10):
-            await ctx.send("https://images-wixmp-ed30a86b8c4ca887773594c2.wixmp.com/f/ead65e88-502c-4a97-9113-44a80af5452f/de18pej-e3d266e1-d6cc-4b10-b9bd-666489904474.png/v1/fill/w_600,h_829,q_80,strp/pecorine_by_wanipurin_de18pej-fullview.jpg?token=eyJ0eXAiOiJKV1QiLCJhbGciOiJIUzI1NiJ9.eyJzdWIiOiJ1cm46YXBwOiIsImlzcyI6InVybjphcHA6Iiwib2JqIjpbW3siaGVpZ2h0IjoiPD04MjkiLCJwYXRoIjoiXC9mXC9lYWQ2NWU4OC01MDJjLTRhOTctOTExMy00NGE4MGFmNTQ1MmZcL2RlMThwZWotZTNkMjY2ZTEtZDZjYy00YjEwLWI5YmQtNjY2NDg5OTA0NDc0LnBuZyIsIndpZHRoIjoiPD02MDAifV1dLCJhdWQiOlsidXJuOnNlcnZpY2U6aW1hZ2Uub3BlcmF0aW9ucyJdfQ.TByWvZjJmEh_a-6NsCb1w4CRwS-9XAavQ1pz4RLc8Cc")
+            await ctx.send("https://i.pinimg.com/736x/ad/bd/1f/adbd1f66b077d5a4ba3de2368c897a66.jpg")
 
         for i in range(100):
             print(f"{i} {msgg}")
@@ -143,7 +143,7 @@ async def pecorine(ctx):
                                                                                                         print("samplestring check done")
         sleep(5)
         for i in range(10):
-            await ctx.send("https://images-wixmp-ed30a86b8c4ca887773594c2.wixmp.com/f/ead65e88-502c-4a97-9113-44a80af5452f/de18pej-e3d266e1-d6cc-4b10-b9bd-666489904474.png/v1/fill/w_600,h_829,q_80,strp/pecorine_by_wanipurin_de18pej-fullview.jpg?token=eyJ0eXAiOiJKV1QiLCJhbGciOiJIUzI1NiJ9.eyJzdWIiOiJ1cm46YXBwOiIsImlzcyI6InVybjphcHA6Iiwib2JqIjpbW3siaGVpZ2h0IjoiPD04MjkiLCJwYXRoIjoiXC9mXC9lYWQ2NWU4OC01MDJjLTRhOTctOTExMy00NGE4MGFmNTQ1MmZcL2RlMThwZWotZTNkMjY2ZTEtZDZjYy00YjEwLWI5YmQtNjY2NDg5OTA0NDc0LnBuZyIsIndpZHRoIjoiPD02MDAifV1dLCJhdWQiOlsidXJuOnNlcnZpY2U6aW1hZ2Uub3BlcmF0aW9ucyJdfQ.TByWvZjJmEh_a-6NsCb1w4CRwS-9XAavQ1pz4RLc8Cc")
+            await ctx.send("https://i.pinimg.com/736x/ad/bd/1f/adbd1f66b077d5a4ba3de2368c897a66.jpg")
 
         for i in range(100):
             print(f"{i} {msgg}")
