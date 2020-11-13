3b60e27d776018fedb811241f474fa49cdc6dd52
SosoM1k0r31z31 <sosoplayzz@gmail.com>
2020-11-13 14:53:33 +0300

    add erina to character db

--- bot.py
+++ bot.py
@@ -1563,9 +1563,13 @@ async def character(ctx, char):
         await ctx.send(charlink("hana"))
     elif char.lower() == "saori":
         await ctx.send(charlink("saori"))
+    elif char.lower() == "erina":
+        await ctx.send(charlink("erina"))
     elif char.lower() == "list":
         await ctx.send("```List of available characters\n\nmako\nanchovy\nyuri\naqua\nmio\nmiho\nhana\nsaori```")
     else:
         await ctx.send("```unknown character\n\nsee %character list for a list of characters```")
+
+
 
 bot.run(TOKEN)
